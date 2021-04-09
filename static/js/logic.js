5ed5fbf88e24ef6464c17ad4553cbb35353c6f6d
Ben <benjatubbs@gmail.com>
2021-04-09 12:49:37 -0700

    Adding flight paths

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,10 +1,19 @@
 // Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-  center: [
-    40.7, -94.5
-  ],
-  zoom: 4
-});
+// let map = L.map("mapid", {
+//   center: [
+//     40.7, -94.5
+//   ],
+//   zoom: 4
+// });
+
+// let map = L.map("mapid", {
+//   center: [
+//     36.1733, -120.1794
+//   ],
+//   zoom: 7
+// });
+
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -17,3 +26,14 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
